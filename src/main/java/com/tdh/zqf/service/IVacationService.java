eee59ceb8112fbc314e423170c6063c17f3e0f4c
zhouqifeng <1767520914@qq.com>
2018-10-17 20:04:58 +0800

    V2.0.1

---
+++ src/main/java/com/tdh/zqf/service/IVacationService.java
@@ -0,0 +1,23 @@
+package com.tdh.zqf.service;
+
+import com.tdh.zqf.model.Leave;
+import com.tdh.zqf.model.User;
+import com.tdh.zqf.model.Vacation;
+
+import java.util.List;
+
+public interface IVacationService {
+
+    public List<Vacation> findAll(int approveid) throws Exception;
+
+    public List<Vacation> findByapplyId(int applyid) throws Exception;
+
+    public Vacation findById(int applyid) throws Exception;
+
+    public List<User> findAdmin() throws Exception;
+
+    public void saveVacation(Leave leave) throws Exception;
+
+    public void modifyById(Integer id, Integer state) throws Exception;
+
+}
---
+++ src/main/java/com/tdh/zqf/service/impl/VacationService.java
@@ -0,0 +1,55 @@
+package com.tdh.zqf.service.impl;
+
+import com.tdh.zqf.mapper.VacationMapper;
+import com.tdh.zqf.model.Leave;
+import com.tdh.zqf.model.User;
+import com.tdh.zqf.model.Vacation;
+import com.tdh.zqf.service.IVacationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VacationService implements IVacationService {
+
+    @Autowired
+    private VacationMapper mapper;
+
+    @Override
+    public List<Vacation> findAll(int approve) throws Exception {
+
+        return mapper.selectAll(approve);
+    }
+
+    @Override
+    public List<Vacation> findByapplyId(int applyid) throws Exception {
+
+        return mapper.selectByapplyId(applyid);
+    }
+
+    @Override
+    public Vacation findById(int Id) throws Exception {
+
+        return mapper.selectById(Id);
+    }
+
+    @Override
+    public List<User> findAdmin() throws Exception {
+
+        return mapper.selectAdmin();
+    }
+
+    @Override
+    public void saveVacation(Leave leave) throws Exception {
+
+        mapper.insertVacation(leave);
+    }
+
+    @Override
+    public void modifyById(Integer id, Integer state) throws Exception {
+
+        mapper.updateById(id, state);
+    }
+
+}
